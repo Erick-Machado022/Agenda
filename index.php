11d387de6491e2ee4f31529a8dba1057156a8b18
Erick <erickmachado050@gmail.com>
2026-05-17 21:17:39 -0300

    Criando configurações iniciais

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+    include_once("config/url.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aenda de Contatos</title>
+
+    <!-- LINK BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap.rtl.min.css" integrity="sha512-4W9f7EnJgM33kD11Ux2XNF9DSpJ7LoQXCvBNhzKTorRSyjPHmjXhHMZMafIYqYvTNWahUk2ulNsarMMLDEv4zA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- LINK ICONS  -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--Link css-->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+
+</head>
+<body>
+    <h1>Testando Agenda</h1>
+    <i class="fas fa-eye"></i>
+</body>
+</html>
