b0ff1d418483cb073a962402acdca11917e17923
Erick <erickmachado050@gmail.com>
2026-05-21 00:20:11 -0300

    Add funcionalidades de visualizar um contato

--- config/process.php
+++ config/process.php
@@ -3,15 +3,40 @@
     include_once("config/connection.php");
     include_once("config/url.php");
 
-    $query = "SELECT * FROM contacts";
+    
+    $id = null;
 
-    $contacts = [];
+    if(!empty($_GET)){
+        $id = $_GET["id"];
+    }
+    
 
-    $stmt = $conn->prepare($query);
+    
+    //Quert para ver apenas um contato
+    if(!empty($id)){
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contato = $stmt->fetch();
+    }else{
+        //Query para todos os contatos
+        $query = "SELECT * FROM contacts";
 
-    $stmt->execute();
+        $contacts = [];
 
-    $contacts = $stmt->fetchAll();
+        $stmt = $conn->prepare($query);
+
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+    }
+
+
+    
+    
+    
+   
 
 
 
---
+++ show.php
@@ -0,0 +1,21 @@
+<?php
+    include_once("templates/header.php");
+    include_once("config/process.php");
+    include_once("config/url.php");
+
+    if(isset($_SESSION['msg'])){
+        $printMsg = $_SESSION['msg'];
+        $_SESSION['msg'] = "";
+
+    }
+?>
+<div class="container" id="contact-container">
+    <h1 id="main-title"><?= $contato["name"] ?></h1>
+    <p class="bold">Telefone</p>
+    <p><?= $contato["phone"] ?></p>
+    <p class="bold">Observações</p>
+    <p><?= $contato["observations"] ?></p>
+</div>
+<?php
+    include_once("templates/footer.php");
+?>
